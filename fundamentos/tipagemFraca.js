96c744dc94a971aa2a2077640a746b78d4c8a854
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-18 16:16:00 -0300

    tipos de variveis usando o [ console.log(typeof variavel) ]

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,9 @@
+let qualquer = 'legal'
+console.log(qualquer)
+console.log(typeof qualquer)    //typeof exibe o tipo da variavel
+
+qualquer = 21.251515
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genericos e siglas na variavel
