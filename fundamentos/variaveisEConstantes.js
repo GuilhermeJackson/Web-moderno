0f0852d406b3a6548dffbb06653c33e40784d268
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-18 16:06:06 -0300

    variaveis var e let e constante const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3   //var permite criar variaveis com mesmo nome  
+let b = 4   //let não permite criar variaveis com mesmo nome
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300 // variaveis podem ser alteradas os valores
+b = 400
+
+console.log(a,b)
+
+const c = 5
+// c = 50   // da erro pq const não altere
+console.log(c) 
