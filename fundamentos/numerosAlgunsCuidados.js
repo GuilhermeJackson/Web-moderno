afb0f9d641800b98755d575663601e685f9e3a3a
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-18 16:54:45 -0300

    algumas dicas para valores numericos para JS

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,4 @@
+console.log(7 / 0 , 'da como infinito')
+console.log(7 / 0.00001 , 'em vez de dividir ele aumenta')
+console.log("Show!" * 2)    //Não é exibido duas vezes o show diferente de outra linguagens
+console.log(0.1 + 0.7)      // calculos geream algumas imprecisões nos pontos flutuantes
