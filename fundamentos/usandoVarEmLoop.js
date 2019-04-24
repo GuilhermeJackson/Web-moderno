71f49553d698a29f88fa34d59d654eb5c9b972b4
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-24 15:14:05 -0300

    loop: usando var para poder imprimir fora do escopo

---
+++ fundamentos/usandoVarEmLoop.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < 10; i++){   //Enquanto var i for menor que 10 encrementa mais 1
+    console.log('i =',i)    //imprimi os encrementos
+}
+
+console.log('i fora do for =', i)   //imprimir o resultado onde o i para o loop
+
+//Se fosse let i, não daria para imprimir fora do escopo pois i nao seria global
