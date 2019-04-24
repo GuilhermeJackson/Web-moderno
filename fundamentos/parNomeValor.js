1b25be7f480f37221efd1d471b1f8fedec75525f
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-24 20:06:02 -0300

    Contexto léxico: é o local fisico onde o código foi definido

---
+++ fundamentos/parNomeValor.js
@@ -0,0 +1,22 @@
+// par nome/valor
+const saudacao = 'Opa'  // contexto léxico 1. q local fisico do código foi definido
+
+function exec() {
+    const saudacao = 'Falaaa'   // contexto léxico 2
+    return saudacao
+}
+
+//Objetos são grupos aninhados de pares nome/valores
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito LEGAL!!!',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
