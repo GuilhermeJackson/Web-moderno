ae24398dfbc3ef8a305cc46398809183966a2a11
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-24 15:52:38 -0300

    Loop com array usando a variavel var - DA RUIM

---
+++ fundamentos/usandoVarEmLoop2.js
@@ -0,0 +1,10 @@
+const funcs = []    //criando um array
+
+for (var i = 0; i < 10; i++) {
+    funcs.push(function(){  //push cria um arrau novo e function() cria uma funcao nao definida que imprimi o i
+        console.log(i)
+    })
+}
+
+funcs [2]() //Era pra retonar 1 mas o i não é respeitado e retorna onde o loop para
+funcs [8]() //Era pra retonar 7 mas o i não é respeitado e retorna onde o loop para
