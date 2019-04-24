a95fde340886cd97383cb8aa1f78f5fe11e275d3
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-24 15:37:01 -0300

    Loop suando let nao da para imprimir fora do escopo

---
+++ fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++){
+    console.log('i =', i)   //variavel i só estara disponivel dentro do bloco
+}
+
+//console.log('i fora ='i)    // da erro por i ser let e nao var. FLW VLW
