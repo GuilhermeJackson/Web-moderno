3f1cab4ac19b58d6315945c342d1bc2c73d92b5a
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-04-27 10:54:41 -0300

    Operadores de atribuicao: adicao, subtracao, multiplicacao, divisao e módulo que retorna 0 para resultado par e 1 para resultado impar

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a  // b = b + a    Acrescentar o valor de 'a' a variavel 'b'
+console.log('b = b + a =', b)
+
+b -= 4  // b = b - 4
+console.log('b = b - a =', b)
+
+b *= a  // b = b * a
+console.log('b = b * a =', b)
+
+b /= a  // b = b / a
+console.log('b = b / a =', b)
+
+b %= 2  // b = b % 2        % é o módulo que se o resultado for par é 0 e se impar é 1
+console.log('b = b % 2 =', b)
