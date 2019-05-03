b89b0f88d2b6a1b41c985e04c72c26d384d4158e
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-05-03 16:27:25 -0300

    Operadores Ternarios: operador com 3 operando usando arrow pra true/false

---
+++ fundamentos/ternario.js
@@ -0,0 +1,9 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.5))
+
+
+
+//operador ternario é o operador com 3 operandos (nota >= 7 ? 'Aprovado' : 'Reprovado')
+//=> significa arrow function
