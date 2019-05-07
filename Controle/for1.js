275d453fda40c0884bbe8abd851a0026f3e3b551
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-05-07 14:39:44 -0300

    For: usando incremento para percorrer os resultados no while, for e array

---
+++ Controle/for1.js
@@ -0,0 +1,12 @@
+let contador = 1
+while (contador <= 10) {
+    console.log(`contador ${contador}`)
+contador++
+}
+
+for(let i = 1; i <=10; i++)
+    console.log(`For: ${i}`)
+
+const nota = [6.5, 6.8, 8, 12]
+    for(let g = 0; g < nota.length; g++)
+    console.log(`Notas: ${nota[g]}`)    //nao esquecer dos [g]
