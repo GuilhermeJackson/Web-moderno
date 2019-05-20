495bcbce8e56b27602d48a6d4c81c4b594588beb
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-05-20 15:27:54 -0300

    Contexto léxico: ao criar uma variavel global e uma variavel local dentro de uma function com o mesmo nome q a variavel global e executar a function, é imprimido o variavel global e nao a local

---
+++ Funcao/contextoLexico.js
@@ -0,0 +1,12 @@
+const valor = 'Global'
+
+function minhaFuncao() {
+    console.log(valor)
+}
+
+function exec() {
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec()  //exec pucha o valor GLOBAL
