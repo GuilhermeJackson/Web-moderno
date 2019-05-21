7b3fbfa686a2f3f3d9059b5a7fb57530c9f18223
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-05-21 08:06:55 -0300

    IIFE = Invocar uma funcao expression imediatamente: cria uma funcao anonima entre parenteses para fugir do escopo global

---
+++ Funcao/iife.js
@@ -0,0 +1,8 @@
+// IIFE -> Immediately Invoked Function Expression
+// Invocar uma funcao expression imediatamente
+// Bastante usada
+
+(function() {
+    console.log('Será executado na hora!!!')
+    console.log('Foge do escopo mais abrangente (foge do escopo global).')
+})()
