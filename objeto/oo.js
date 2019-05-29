d6bf06bbbad2440995fcdefa99a2a5c0ea17a72f
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-05-28 23:51:06 -0300

    Principios de Orientacao a Objeto: abstracao, encapsulamento, heranca e polimorfismo (código nao executavel)

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL
+
+// Procedural // funcao
+processamento(valor1, valor2, valor3)
+
+// OO
+Object = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento()  // Foco passou a ser o objeto
+
+//Principios importantes:
+//1. abstracao = Trazer alguma coisa do mundo real pro sistema com informações relevantes
+//2. encapsulamento = esconder algumas coisas do objeto dentro do mesmo e exibir apenas o necessario
+//3. herança (prototype) = quando é herdado alguma funcao ou objeto de alguem (é alguma coisa / tem alguma coisa)
+//4. polimorfismo = multiplas formas. atribuir alguma coisa na classe pai
